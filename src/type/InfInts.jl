670f11e79ef62192e23578586a630078e12c1ace
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-21 06:53:12 -0400

    Update InfInts.jl

--- src/type/InfInts.jl
+++ src/type/InfInts.jl
@@ -1,1 +1,26 @@
+abstract ExtendedInteger{T} <: Signed
 
+abstract   FiniteInteger{T} <: ExtendedInteger{T}
+abstract InfiniteInteger{T} <: ExtendedInteger{T}
+
+abstract     ZeroInteger{T} <: FiniteInteger{T}
+abstract     UnitInteger{T} <: FiniteInteger{T}
+abstract    OtherInteger{T} <: FiniteInteger{T} 
+
+typealias SpecialInteger{T} Union{ZeroInteger{T}, UnitInteger{T}, InfiniteInteger{T}}
+
+
+immutable FiniteInt{T<:Signed} <: FiniteInteger{T}
+    val::T
+
+    FiniteInt{T}(val::T) = new(val)
+end
+
+FiniteInt{T<:Signed}(val::T) = FiniteInt{T}(val)
+
+
+@inline function Base.show{T<:Integer}(io::IO, x::FiniteInt{T})
+    print(io, dec(x.val))
+end
+
+    
